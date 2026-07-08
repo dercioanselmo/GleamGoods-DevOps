ecb7dbdfa032d073b7c5c2d3fbc0a7984fa71e80
Dercio Anselmo <dercio@branchdev.io>
2026-07-08 09:41:00 +0400

    Remote Backend, VPC and CICD to VPC

---
+++ 02_VPC/c1-versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">=1.0.0" #Terreform CLI version
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0" #Provider version
+    }
+  }
+  # Remote Backend
+  backend "s3" {
+    bucket = "tfstate-dev-us-east-1-1v8wcs"
+    key = "GleamGoods/vpc/terraform.tfstate"
+    region = "us-east-1" # HArdcoded Variable because values from variables is still not allowed in the terraform block. 
+    encrypt = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
